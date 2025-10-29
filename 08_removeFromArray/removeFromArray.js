d408eb995000053bc030ffdc728a596501e63468
Pyrox004 <abderaghman.calendly@gmail.com>
2025-10-29 23:00:51 +0100

    finished exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+    for(i = 0; i < arr.length; i++) {
+        if(numbers.includes(arr[i])) {
+            arr.splice(i, 1)
+            i--
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
