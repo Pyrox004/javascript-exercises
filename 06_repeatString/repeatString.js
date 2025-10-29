982b31e5ee66a8a3f11d9a64d02481b2468a6ed7
Pyrox004 <abderaghman.calendly@gmail.com>
2025-10-29 20:01:35 +0100

    completed exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) return Error
+    let container = ''
+    for(i = 0; i < num; i++) {
+        container += str
+    }
+    return container
 };
 
 // Do not edit below this line
