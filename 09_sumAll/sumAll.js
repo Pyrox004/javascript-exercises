e78ade65efa5392e9b84b0ec01701828faabb8cd
Pyrox004 <abderaghman.calendly@gmail.com>
2025-10-30 01:59:23 +0100

    Finished with exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a)
+    || !Number.isInteger(b) 
+    || a < 0
+    || b < 0) {
+        return 'ERROR'
+    }
 
+    let loopArr = []
+    
+    for(let i = 0; i <= Math.abs(b - a); i++) {
+        if(a < b) {
+            loopArr.push(a + i)
+        } else {
+            loopArr.push(a - i)
+        }
+    }
+    return loopArr.reduce((sum, current) => sum + current, 0)
 };
 
 // Do not edit below this line
