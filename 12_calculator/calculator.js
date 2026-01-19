afb1217f267eb6eada38252f30612e8b4b8e8979
Pyrox004 <abderaghman.calendly@gmail.com>
2026-01-19 03:38:04 +0100

    feat: finished exercise 12 of objects lessons

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if(!arr || !arr.length) {
+    return null;
+  }
 
+  let result = 1;
+  for(let a of arr) {
+    result *= a; 
+  } 
+  
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  for (let i = 2; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
