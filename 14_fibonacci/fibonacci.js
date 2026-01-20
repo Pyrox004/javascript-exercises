be98687ed2f754dff7b65ab07c87e74dcfaf1539
Pyrox004 <abderaghman.calendly@gmail.com>
2026-01-20 14:20:49 +0100

    feat: completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let number 
 
+    if(n !== Number) {
+        number = parseInt(n)
+    } else number = n
+
+    if (number < 0)
+        return "OOPS"
+
+    if (number == 0)
+        return 0
+
+    let firstPrev = 1
+    let secondPrev = 0
+
+    for(i = 2; i <= number; i++) {
+        let current = firstPrev + secondPrev
+        secondPrev = firstPrev
+        firstPrev = current
+    }
+
+    return firstPrev
 };
 
 // Do not edit below this line
