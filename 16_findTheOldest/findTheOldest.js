7b02604c54d6cf8cb126f68e510707df84c1c453
Pyrox004 <abderaghman.calendly@gmail.com>
2026-01-21 02:17:21 +0100

    feat: completed exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currentPerson) => {
+        const oldestAge = (oldest.yearOfDeath || 2026) - oldest.yearOfBirth;
+        const currentPersonAge = (currentPerson.yearOfDeath || 2026) - currentPerson.yearOfBirth;
+
+    return oldestAge < currentPersonAge ? currentPerson : oldest   
+    })
+
 
 };
 
